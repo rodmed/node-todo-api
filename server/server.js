b57f244b08322ec597ce545e7f5364f90a3346ee
Rodrigo Medeiros <rodmed1985@gmail.com>
2017-03-01 08:30:44 -0300

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -12,17 +12,27 @@ app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
     var todo = new Todo({
-        text: req.body
+        text: req.body.text
     })
 
     todo.save().then( (doc) => {
         res.send(doc);
     }, (err) => {
-        res.status(400).send(e);
+        res.status(400).send(err);
     });
 });
+
+app.get('/todos', (req, res) => {
+    Todo.find({}).then ( (todos)=>{
+        res.send({todos})
+    }, (err) => {
+        res.status(400).send(e);
+    })
+})
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
  
+
+module.exports = {app};
