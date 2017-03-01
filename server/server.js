4c20240cf776c4f03c67e4d2f7be81b2cb2f531f
Rodrigo Medeiros <rodmed1985@gmail.com>
2017-03-01 17:32:15 -0300

    Adding PATCH /todos:id route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require ('./db/mongoose');
 var {User} = require ('./models/user');
@@ -74,5 +76,29 @@ app.listen(PORT, () => {
     console.log(`Started on port ${PORT}`);
 });
  
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true} ).then( (todo) => {
+        if (!todo) {
+            res.status(404).send();
+        }
+        res.status(200).send({todo});
+    }, (err) => { 
+        res.status(400).send();
+    });
+});    
 
 module.exports = {app};
